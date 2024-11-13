38836517511d1a984a50af1b37ee2e83049be2c5
gunnargriffith <122704565+gunnargriffith@users.noreply.github.com>
2024-11-13 10:58:48 -0700

    login jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
+
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
-    </main>
+    <main>
+  <fieldset className="loginMain">
+    <h4>Please login as a Game Master or Player:</h4>
+
+    <label className="checkBoxLabel" htmlFor="GameMaster">Game Master</label>
+    <input
+      className="textbox"
+      type="checkbox"
+      id="GameMaster"
+      name="gmCheckbox"
+      value="Game Master"
+      style={{ marginRight: '20px' }}
+    />
+
+    <label className="checkBoxLabel" htmlFor="Player">Player</label>
+    <input className="textbox" type="checkbox" id="Player" name="pCheckbox" value="Player" />
+
+    <br />
+    <br />
+
+    <div className="input-group mb-3">
+      <span className="input-group-text">@</span>
+      <input className="form-control" type="text" placeholder="your@email.com" />
+    </div>
+
+    <div className="input-group mb-3">
+      <span className="input-group-text">🔒</span>
+      <input className="form-control" type="password" placeholder="password" />
+    </div>
+
+    <button type="submit" onClick={() => window.location.href = 'playernotes.html'}>
+      Login
+    </button>
+    
+    <button type="submit" onClick={() => window.location.href = 'playernotes.html'}>
+      New Profile
+    </button>
+
+  </fieldset>
+</main>
   );
 }
--- src/worldlore/worldlore.jsx
+++ src/worldlore/worldlore.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './worldlore.css'
 
 export function WorldLore() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>worldlore displayed here</div>
+    <main>
+      <fieldset className="loreBox" >
+        <h2 className="sectionTitle">World Lore</h2>
+        <div className="loreEntryContainer">
+          <a href="#" class="loreEntry">General World History</a> <a className="accessText"> --Access: All Players--</a>
+        </div>
+        <div className="loreEntryContainer">
+          <a href="#" className="loreEntry">Pantheon</a> <a className="accessText"> --Access: Clerics/Paladins--</a>
+        </div>
+        <div className="loreEntryContainer">
+          <a href="#" className="loreEntry">The West Fold</a> <a className="accessText"> --Access: Humans/Dwarfs--</a>
+        </div>
+        <div className="loreEntryContainer">
+          <a href="#" className="loreEntry">Waterdeep Guilds</a> <a className="accessText"> --Access: Ceil Aldoilea--</a>
+        </div>
+      </fieldset>
     </main>
   );
 }
