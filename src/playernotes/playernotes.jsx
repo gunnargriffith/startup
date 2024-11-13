6e30dd2228d10ad9cee519ab468b252e2b026f66
gunnargriffith <122704565+gunnargriffith@users.noreply.github.com>
2024-11-12 21:53:24 -0700

    adding jsx files

---
+++ src/playernotes/playernotes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function PlayerNotes() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>playernotes displayed here</div>
+    </main>
+  );
+}
---
+++ src/worldlore/worldlore.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function WorldLore() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>worldlore displayed here</div>
+    </main>
+  );
+}
