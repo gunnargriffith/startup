7debefe4a387c1b8d0bb25b4c60085b335580483
gunnargriffith <122704565+gunnargriffith@users.noreply.github.com>
2024-11-13 10:49:35 -0700

    playernotes jsx

--- src/playernotes/playernotes.jsx
+++ src/playernotes/playernotes.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './playernotes.css'
 
 export function PlayerNotes() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>playernotes displayed here</div>
+    <main>
+        <div className="notesBox">
+          <fieldset>
+            <h2 className="NotesTitle">Player Notes</h2>
+
+            <a className="noteEntry" href="#">Yosai's Notes</a>
+            <br  />
+            <a className="noteEntry" href="#">Ciel's Journal</a>
+            <br  />
+            <a className="noteEntry" href="#">Eliane Notes</a>
+          
+          </fieldset>
+        </div>
     </main>
   );
 }
